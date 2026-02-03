391618228dfecd42a148598681057979af7231c1
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2026-02-03 08:59:53 +0200

    imports setup

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const pool = mysql.createPool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+app.listen(5050, () => {
+  console.log("http://localhost:5050");
+});
