c048b058ec0bac0be1c28bed4377772d1e1c2afe
Tamrynripepi00 <tamrynripepi00@gmail.com>
2026-02-05 12:45:20 +0200

    added messages

--- Backend/index.js
+++ Backend/index.js
@@ -19,6 +19,57 @@ const pool = mysql.createPool({
   database: process.env.DB_DATABASE,
 });
 
+// Get Attendance
+app.get("/attendance", async (req, res) => {
+  const [rows] = await pool.query(
+    "SELECT * FROM employee_data.attendance"
+  );
+
+  res.json({
+    message: "Attendance route accessed",
+    status: "GET request successful",
+    data: rows
+  });
+});
+
+// POST
+app.post("/attendance", async (req, res) => {
+  const { employee_id, attendance_date, status } = req.body;
+
+  await pool.query(
+    `INSERT INTO employee_data.attendance 
+     (employee_id, attendance_date, status)
+     VALUES (?, ?, ?)`,
+    [employee_id, attendance_date, status]
+  );
+
+  res.json({
+    message: "Attendance accessed",
+    status: "Attendance record added"
+  });
+});
+
+
+// PATCH
+app.patch("/attendance/:id", async (req, res) => {
+  const { status } = req.body;
+  const { id } = req.params;
+
+  console.log(req.body); 
+  
+  await pool.query(
+    `UPDATE employee_data.attendance
+     SET status = ?
+     WHERE attendance_id = ?`,
+    [status, id]
+  );
+
+  res.json({
+    message: `Attendance saved`,
+    updated_id: id,
+    new_status: status
+  });
+});
 
 app.listen(5050, () => {
   console.log("http://localhost:5050");
